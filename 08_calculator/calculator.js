4467766832fc2923be8c94665290a4a048dbb0bc
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-20 00:14:03 +1000

    completed task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
+const add = function(numOne, numTwo) {
+	result = 0;
+  result = numOne + numTwo;
+  return result;
+};
+
+const subtract = function(numOne, numTwo) {
+	result = 0;
+  result = numOne - numTwo;
+  return result;
+};
+
+const sum = function(array) {
+  let result = 0;
+  for (let i =0;i<array.length; i++){
+    result += array[i];
+  }
+	return result
+};
+
+const multiply = function(array) {
+  let result = 1;
+  for (let i =0;i<array.length; i++){
+    result *= array[i];
+  }
+  return result;
+};
+
+const power = function(numOne, numTwo) {
+  let result = 0;
+  result = numOne ** numTwo;
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i =number;i>1; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
