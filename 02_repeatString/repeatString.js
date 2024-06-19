c5f920555ce7852c73b62bd8947ff66e64087693
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-19 22:17:38 +1000

    completed task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    for (i = 0; i<num; i++){
+        result+=string;
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
