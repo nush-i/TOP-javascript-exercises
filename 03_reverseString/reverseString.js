7cde24353ba4d261a614dd4a8e1b4848653d7227
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-19 22:37:56 +1000

    completed task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    for (let i =string.length -1; i>=0; i--){
+        result+=string[i];
+    }
+    return result;
 };
 
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
