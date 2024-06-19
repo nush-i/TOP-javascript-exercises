416f8b10d7cc1e2830620f4e4e07c5a3b4f07b95
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-19 23:56:07 +1000

    completed task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = 0;
+  result = (fahrenheit - 32) * 5/9;
+  return Math.round(result *10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = 0;
+  result = (celsius*9/5)+32;
+  return Math.round(result*10)/10;
 };
 
 // Do not edit below this line
