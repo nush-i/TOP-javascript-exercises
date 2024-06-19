7a5c7306587d4d57abb0852880b3767cf3e4b432
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-20 00:32:13 +1000

    completed task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const char = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const newString =char.split('').reverse().join('');
+    return char === newString;
+   
 };
 
 // Do not edit below this line
