755936c73dd1f4b95a74714470e9cd93a6c848d0
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-19 23:23:08 +1000

    completed task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0|| numTwo <0 || !Number.isInteger(numOne)|| !Number.isInteger(numTwo)){
+        return 'ERROR'
+    }
+    let total = 0;
+    let i = numOne;
+    if (numOne>numTwo){
+        while (i>=numTwo){
+            total+=i;
+            i--;
+        }
+        
+    }
+    else{
+        while (i<=numTwo){
+            total+=i;
+            i++;
+        }
+    }
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
