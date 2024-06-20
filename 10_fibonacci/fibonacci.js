67fa94cd7df65da4cff3437bf0a38f445de130f8
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-20 11:06:52 +1000

    completed task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number<0){
+        return 'OOPS';
+    }
+    if (number === 0 ||number === '0'){
+        return 0;
+    }
+    if (number === 1){
+        return a;
+    }
+    if (number ===2){
+        return b;
+    }
+    let a=0;
+    let b=1;
+    for (let i = 1;i<number; i++){
+        let next = a+b;
+        a=b;
+        b=next;
+    }
+    return b;
 };
 
 // Do not edit below this line
