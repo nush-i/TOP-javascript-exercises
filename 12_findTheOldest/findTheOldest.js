b81be427063dd3eae5746b4e45da97b14907e531
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-23 15:59:47 +1000

    completed task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findAge = function(birthDate, deathDate){
+    if (!deathDate) {
+        deathDate = new Date().getFullYear();
+    
+    }
+    return deathDate - birthDate;
+};
+
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, findPerson) =>{
+        const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = findAge(findPerson.yearOfBirth, findPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return findPerson;
+        }
+        else {
+            return oldest;
+        }
+    });
 
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
